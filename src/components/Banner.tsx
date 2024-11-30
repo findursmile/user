0f64f9cbce460fbc68b86809a625bfd7fd5ebce5
Mohan Raj <praem1990@gmail.com>
2024-12-01 02:22:13 +0530

    Sync event details

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,17 +1,18 @@
 import { CalendarIcon } from '@heroicons/react/24/outline'
 
-function Banner({onViewChange}: {onViewChange: Function}) {
+function Banner({onViewChange, event}: {onViewChange: Function, event?: {title: string}}) {
   return (
     <>
         <section
-            className="w-full bg-[url('https://j.gifs.com/KeGkG9.gif')] bg-cover bg-center min-h-80 rounded-lg mt-6 relative"
-            style={{backgroundImage: ""}}
+            className={`w-full ${event?.cover_photo ? '' : "bg-[url('https://j.gifs.com/KeGkG9.gif')]"} bg-cover bg-center min-h-80 rounded-lg mt-6 relative`}
+            style={{backgroundImage: event?.cover_photo}}
         >
             <div className="absolute p-5 left-0 bottom-0 text-white">
                 <div className="flex align-center font-sm pb-2">
-                    <CalendarIcon className="w-4 inline-block mr-1"/> Date goes here
+                    <CalendarIcon className="w-4 inline-block mr-1"/>
+                    {event?.event_date}
                 </div>
-                <h3 className="text-2xl font-bold">Event Name Goes Here</h3>
+                <h3 className="text-2xl font-bold">{event?.title}</h3>
                 <div className="flex gap-1">
                     <button
                         className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
